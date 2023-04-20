0773043e00790d3904df1f04b6f33f51ad1864f0
joaquin <joaquindiaz0000@gmail.com>
2023-04-20 09:43:45 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// let grande = document.querySelector(".grande");
+// let punto  = document.querySelectorAll(".punto");
+
+// punto.forEach( (cadaPunto , i) => {
+//     punto[i].addEventListener("click", () => {
+
+//         let posicion = i
+//         let operacion = posicion * -50 
+
+//         grande.style.transform = `translateX(${operacion}%)`;
+
+//         punto.forEach( (cadaPunto , i) => {
+//             punto[i].classList.remove("active")
+//         })
+//         punto[i].classList.add("active")
+//     })
+// })
