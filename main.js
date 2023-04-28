5121c77951754caac69ae887f20457302f1f85a0
joaquin <joaquindiaz0000@gmail.com>
2023-04-27 23:29:04 -0300

    PreEntrega2

--- main.js
+++ main.js
@@ -15,9 +15,10 @@
 //         punto[i].classList.add("active")
 //     })
 // })
-let nav = document.querySelector(".nav");
-let botonAbrir = document.querySelector(".open");
-let botonCerrar = document.querySelector(".close");
+// MENU HAMBURGUESA
+const nav = document.querySelector(".nav");
+const botonAbrir = document.querySelector(".open");
+const botonCerrar = document.querySelector(".close");
 
 botonAbrir.addEventListener("click", () => {
     nav.classList.add("active")
@@ -26,10 +27,23 @@ botonCerrar.addEventListener("click", () => {
     nav.classList.remove("active")
 })
 
+// MODAL CARRITO
+const open = document.querySelector("#openCarrito")
+const close = document.querySelector("#closeCarrito")
+const modal = document.querySelector(".carrito")
+
+open.addEventListener("click", () => {
+    modal.classList.add("active")
+})
+
+close.addEventListener("click", () => {
+    modal.classList.remove("active")
+})
+
 // CARRUSEL 3 CUOTAS
-let leftArrow = document.querySelector(".leftArrow");
-let rightArrow = document.querySelector(".rightArrow");
-let parrafos = document.querySelectorAll(".parrafos");
+const leftArrow = document.querySelector(".leftArrow");
+const rightArrow = document.querySelector(".rightArrow");
+const parrafos = document.querySelectorAll(".parrafos");
 
 leftArrow.addEventListener("click", () =>  mover(-1));
 rightArrow.addEventListener("click", () =>  mover(1));
@@ -46,5 +60,91 @@ function mover(numero) {
 
     parrafos[currentElement-1].classList.toggle("parrafo-active");
     parrafos[value-1].classList.toggle("parrafo-active");
+}
+
+// PRE ENTREGA 2
+
+const Productos = [
+
+    {
+        nombre: "Nike SB Bruin React",
+        color: "Negro",
+        precio: 49000,
+    },
+
+    {
+        nombre: "Nike Court Legacy Canvas",
+        color: "Blanco",
+        precio: 30000,
+    },
+
+    {
+        nombre: "Nike Air Force 1 '07",
+        color: "Blanco",
+        precio: 53000,
+    },
+
+    {
+        nombre: "Nike ZoomX Invincible Run Flyknit 2",
+        color: "Negro",
+        precio: 89000,
+    },
+
+    {
+        nombre: "Nike Dunk Low Retro Premium",
+        color: "Negro",
+        precio: 57000,
+    }
+
+]
+
+let accion = parseInt(prompt("Bienvenidos a Nike.ar. Que desea realizar? \n1. Mostrar stock \n2.Filtrar por mas caras  \n3.Filtrar por mas baratas \n4.Filtrar por color negro \n5.Filtrar por color blanco"))
+
+
+function mostrarProductos(){
+    Productos.forEach( prodcuto => {
+        console.log(prodcuto.nombre)
+        console.log("Precio: " + prodcuto.precio + " pesos.")
+    });
+}
+
+function caras() {
+    Productos.sort((a,b) => b.precio - a.precio);
+    console.log(Productos)
+}
+
+function baratas() {
+    Productos.sort((a,b) => a.precio - b.precio);
+    console.log(Productos)
+}
+
+function colorNegro() {
+    const negro = Productos.filter((producto) => producto.color === "Negro")
+    console.log(negro);
+}
+
+function colorBlanco() {
+    const blanco = Productos.filter((producto) => producto.color === "Blanco")
+    console.log(blanco);
+}
+
+switch (accion) {
+    case 1:
+        mostrarProductos()
+        break;
+    case 2:
+        caras()
+        break;
+    case 3:
+        baratas()
+        break;
+    case 4:
+        colorNegro()
+        break;
+    case 5:
+        colorBlanco() 
+        break;
+    default:
+        alert("Valor invalido")        
 }
 
